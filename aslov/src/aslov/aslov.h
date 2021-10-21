ab886966ab61d362ab9648ec920ffc1452bec65f
aleksey slovesnov <slovesnov@yandex.ru>
2021-10-21 05:53:41 +0300

    add printv, printvi functions.Move content of help.txt to help.html

--- aslov/src/aslov/aslov.h
+++ aslov/src/aslov/aslov.h
@@ -131,6 +131,28 @@ void aslovPrints(const char separator,A const &a, B const &... b) {
 #define printai(...) aslovPrintHelp(0,formata(__VA_ARGS__),__FILE__,__LINE__,__func__);
 #define printzi(...) aslovPrintHelp(0,formatz(__VA_ARGS__),__FILE__,__LINE__,__func__);
 
+/* https://stackoverflow.com/questions/1872220/is-it-possible-to-iterate-over-arguments-in-variadic-macros
+ * many modifications aslov
+ */
+#define ASLOV_PRINT_VARIABLE(a) printan(formatz(#a," = ",a))
+#define ASLOV_PRINT_VARIABLE_I(a) printai(formatz(#a," = ",a))
+#define ASLOV_CONCATENATE(a, b)   a##b
+#define ASLOV_FOR_EACH_NARG(...) ASLOV_FOR_EACH_NARG_(__VA_ARGS__, ASLOV_FOR_EACH_RSEQ_N())
+#define ASLOV_FOR_EACH_NARG_(...) ASLOV_FOR_EACH_ARG_N(__VA_ARGS__)
+#define ASLOV_FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N
+#define ASLOV_FOR_EACH_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define ASLOV_FOR_EACH_1(f,f1,x,...) f1(x)
+#define ASLOV_FOR_EACH_2(f,f1,x,...) f(x)ASLOV_FOR_EACH_1(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_3(f,f1,x,...) f(x)ASLOV_FOR_EACH_2(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_4(f,f1,x,...) f(x)ASLOV_FOR_EACH_3(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_5(f,f1,x,...) f(x)ASLOV_FOR_EACH_4(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_6(f,f1,x,...) f(x)ASLOV_FOR_EACH_5(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_7(f,f1,x,...) f(x)ASLOV_FOR_EACH_6(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH_8(f,f1,x,...) f(x)ASLOV_FOR_EACH_7(f,f1,__VA_ARGS__)
+#define ASLOV_FOR_EACH(N,f,f1,x,...) ASLOV_CONCATENATE(ASLOV_FOR_EACH_,N)(f,f1, x, __VA_ARGS__)
+#define ASLOV_PRINTV(f1,...) ASLOV_FOR_EACH(ASLOV_FOR_EACH_NARG(__VA_ARGS__), ASLOV_PRINT_VARIABLE, f1, __VA_ARGS__)
+#define printv(...) ASLOV_PRINTV(ASLOV_PRINT_VARIABLE,__VA_ARGS__)
+#define printvi(...) ASLOV_PRINTV(ASLOV_PRINT_VARIABLE_I,__VA_ARGS__)
 
 //output info to log file printlog("%d %s",1234,"some")
 #define printlog(...)  aslovPrintHelp(1,format(__VA_ARGS__),__FILE__,__LINE__,__func__);
