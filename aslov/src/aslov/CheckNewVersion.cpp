c6a923725f83152c95627f9b5c9bb5ac4a595423
aleksey slovesnov <slovesnov@yandex.ru>
2026-09-13 07:33:14 +0300

    1

--- aslov/src/aslov/CheckNewVersion.cpp
+++ aslov/src/aslov/CheckNewVersion.cpp
@@ -32,6 +32,8 @@ void CheckNewVersion::routine() {
   if (curl) {
     std::string url =
         "https://api.github.com/repos/slovesnov/" + getApplicationName() + "/releases/latest";
+    // std::string url =
+    //     "https://api.github.com/repos/slovesnov/stopwatch/releases/latest";
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
     struct curl_slist *headers = NULL;
@@ -53,11 +55,14 @@ void CheckNewVersion::routine() {
 
     if (res == CURLE_OK) {
       try {
+        // pr(readBuffer)
         auto j = nlohmann::json::parse(readBuffer);
         if (j.is_object() && j.contains("tag_name")) {
           auto version = j["tag_name"].get<std::string>();
+          // pr(version)
           if (m_version != version) {
             m_message = j.value("body", "");
+            // pr(m_message)
             gdk_threads_add_idle(m_callback, NULL);
           }
         } else {
@@ -70,6 +75,29 @@ void CheckNewVersion::routine() {
   }
   // Network Error
 }
+
+/*
+void CheckNewVersion::routine() {
+	GFile *f = g_file_new_for_uri(m_versionUrl.c_str()); //f is always not null
+	gsize length;
+	char *content = NULL;
+	if (g_file_load_contents(f, NULL, &content, &length, NULL, NULL)) {
+		std::string s(content, length); //content is not null terminated so create std::string
+		setNumericLocale(); //dot interpret as decimal separator
+		char *p;
+		double v = strtod(s.c_str(), &p); //std::stod(s) throws exception if cann't parse double
+		//symbol \r or \n should goes after version in version files
+		bool ok = p != s.c_str() && (*p == '\r' || *p == '\n');
+		if (ok && v > m_version) {
+			//19oct25 file in utf8
+			m_message = s;
+			gdk_threads_add_idle(m_callback, NULL);
+		}
+	}
+	g_free(content);
+	g_object_unref(f);
+}
+*/
 
 CheckNewVersion::~CheckNewVersion() { g_thread_join(m_newVersionThread); }
 #endif /* NOGTK */
